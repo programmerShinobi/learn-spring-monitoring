fe631386d009617fe5d3592510b2fc1496673048
programmerShinobi <shinobiprogrammer@gmail.com>
2026-04-26 07:37:28 +0700

    [add] setup project

---
+++ src/test/java/programmershinobi/monitoring/BelajarSpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package programmershinobi.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
