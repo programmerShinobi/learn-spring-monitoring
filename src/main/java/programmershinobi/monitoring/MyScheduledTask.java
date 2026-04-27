2cd6a4468818f76bb2aab30a7e2f64e2d7d9e030
programmerShinobi <shinobiprogrammer@gmail.com>
2026-04-27 11:35:49 +0700

    [add] learn : actuator web endpoints & scheduled task

---
+++ src/main/java/programmershinobi/monitoring/MyScheduledTask.java
@@ -0,0 +1,16 @@
+package programmershinobi.monitoring;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class MyScheduledTask {
+
+    @Scheduled(fixedRate = 10L, timeUnit = TimeUnit.SECONDS)
+    public void hello() {
+        System.out.println("Hello World :)");
+    }
+
+}
